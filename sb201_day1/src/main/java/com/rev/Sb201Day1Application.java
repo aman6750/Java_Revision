d9a2f55d5f8327b6d6ced75a2fab866b9f61f8d1
Aman <amanyadav6750@gmail.com>
2023-10-14 21:19:05 +0530

    what is hibernate,springboot

---
+++ sb201_day1/src/main/java/com/rev/Sb201Day1Application.java
@@ -0,0 +1,13 @@
+package com.rev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb201Day1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb201Day1Application.class, args);
+	}
+
+}
---
+++ sb201_day1/src/test/java/com/rev/Sb201Day1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb201Day1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
